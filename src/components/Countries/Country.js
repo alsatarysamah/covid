7c3074e9a4aeaf0ee63e6bee3ea87568159d1ef6
samah anwer alsatary <alsatary.samah@yahoo.com>
2022-12-16 21:53:01 +0200

    all countries

--- src/components/Countries/Country.js
+++ src/components/Countries/Country.js
@@ -1,45 +1,65 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./Country.css";
 
 export default function Country() {
+  const [allCountries, setAllCountries] = useState([]);
+
+  const handleAddRecord=(record)=>{
+    console.log({record});
+  }
   useEffect(() => {
-    axios.get("https://api.covid19api.com/summary")
+    axios.get("https://api.covid19api.com/summary").then((data) => {
+      setAllCountries(data.data.Countries);
+      console.log(data.data.Countries);
+    });
   }, []);
   return (
     <>
-      <section class="pricing-section">
-        <div class="container">
-          <div class="row justify-content-md-center mt-3">
-            <div class="col-xl-5 col-lg-6 col-md-8">
-              <div class="section-title text-center title-ex1">
-                <h2>Pricing Tables</h2>
-                <p>
-                  Inventore cillum soluta inceptos eos platea, soluta class
-                  laoreet repellendus imperdiet optio.
-                </p>
-              </div>
-            </div>
-          </div>
+      <section >
+        <div class="container ">
+        <div class="row h-25 mt-5">
+          {allCountries?.map((element) => (
+            
+            <div class="col-md-3 mt-3">
+                <div class="price-card featured">
+                  <h2>Country: {element.Country.slice(0,element.Country.indexOf(" "))} </h2>
+                  {/* <p>Most popular choice</p> */}
+                  <ul class="pricing-offers">
+                    <li>Total Confirmed: {element.TotalConfirmed}</li>
+                    <li>Total Deaths: {element.TotalDeaths}</li>
+                    <li>Total Recovered: {element.TotalRecovered}</li>
 
-          <div class="row h-25">
-            <div class="col-md-4 mt-3">
-              <div class="price-card featured">
-                <h2>Student</h2>
-                {/* <p>Most popular choice</p> */}
-                <ul class="pricing-offers">
-                  <li>6 Domain Names</li>
-                  <li>8 E-Mail Address</li>
-                  <li>8 E-Mail Address</li>
-
-                  <li>8 E-Mail Address</li>
-                </ul>
-                <button class="btn btn-primary  ">Add</button>
-              </div>
+                    <li>Date: {element.Date}</li>
+                  </ul>
+                  <div className="add-btn">
+                    <button class="btn btn-primary btn-lg  " onClick={()=>handleAddRecord(element)}>Add</button>
+                  </div>
+                </div>
             </div>
-          </div>
+          ))}
         </div>
+        </div>
+
       </section>
     </>
   );
 }
+/**  <div class="row h-25 mt-5">
+            <div class="col-md-4 mt-3">
+              <div class="price-card featured">
+                <h2>Country </h2>
+                {/* <p>Most popular choice</p> */
+//       <ul class="pricing-offers">
+//         <li>6 Domain Names</li>
+//         <li>8 E-Mail Address</li>
+//         <li>8 E-Mail Address</li>
+
+//         <li>8 E-Mail Address</li>
+//       </ul>
+//       <div className="d-flex justify-content-center px-auto">
+//       <button class="btn btn-primary  ">Add</button>
+//       </div>
+//     </div>
+//   </div>
+// </div> */
