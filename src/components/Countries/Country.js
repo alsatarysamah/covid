f0fdb1223ee53502858dae31f28499abe313e46b
samah anwer alsatary <alsatary.samah@yahoo.com>
2022-12-19 21:35:51 +0200

    signin

--- src/components/Countries/Country.js
+++ src/components/Countries/Country.js
@@ -15,7 +15,7 @@ const navigate =useNavigate();
     console.log(userInfo);
     if(userInfo.id)
     axios
-      .post("https://covid-19-asac.up.railway.apprecord", {
+      .post("https://covid-19-asac.up.railway.app/record", {
         country: record.Country,
         totalConfirmedCases: record.TotalConfirmed,
         totalDeathsCases: record.TotalDeaths,
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -31,7 +31,7 @@ export default function Signup() {
     }
     
       axios
-        .post("https://covid-19-asac.up.railway.appsignup", { username, password, email })
+        .post("https://covid-19-asac.up.railway.app/signup", { username, password, email })
         .then((data) => {
            sessionStorage.setItem('userInfo', JSON.stringify(data));
           navigate('/signin');
